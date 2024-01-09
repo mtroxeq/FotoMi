24e6b681cb231de42ebf6ea303cc3dbcf747d560
Maciej Szabat/mtroxeq <matroxszabat@gmail.com>
2024-01-09 15:10:44 +0100

    Dodano ikonki i projekt aplikacji w SceneBuilder

--- src/main/java/com/example/fotomi/HelloApplication.java
+++ src/main/java/com/example/fotomi/HelloApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("FotoMi");
         stage.setScene(scene);
         stage.show();
     }
